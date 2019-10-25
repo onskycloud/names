ae9a4276f84b383bc735d611d78a045576fd12c9
Phạm Nguyên Cát <dtpham258@gmail.com>
2019-10-25 16:01:44 +0700

    initialize

---
+++ main.go
@@ -0,0 +1,36 @@
+package names
+
+import (
+	"fmt"
+	"strings"
+)
+
+// GetFirstName get first name
+func GetFirstName(name string) string {
+	if name == "" {
+		return ""
+	}
+	wordArr := strings.Fields(name)
+	return wordArr[0]
+}
+
+// GetLastName get last name
+func GetLastName(name string) string {
+	if name == "" {
+		return ""
+	}
+	wordArr := strings.Fields(name)
+	if len(wordArr) == 1 {
+		return wordArr[0]
+	}
+	return strings.Join(wordArr[1:len(wordArr)], " ")
+
+}
+
+// GetFullName get full name
+func GetFullName(firstName string, lastName string) string {
+	if firstName == "" && lastName == "" {
+		return ""
+	}
+	return fmt.Sprintf("%s %s", firstName, lastName)
+}
